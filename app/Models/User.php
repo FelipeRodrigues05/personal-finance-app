3e064dfa3dfdd3ca334e7711aaa597c4ef4a910c
FelipeRodrigues05 <fehzin_@outlook.com>
2025-08-30 01:24:26 -0300

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Enums\UserRole;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-final class User extends Authenticatable
+final class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
@@ -59,5 +61,10 @@ final class User extends Authenticatable
             'password'          => 'hashed',
             'role'              => UserRole::class,
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role == UserRole::ADMIN;
     }
 }
