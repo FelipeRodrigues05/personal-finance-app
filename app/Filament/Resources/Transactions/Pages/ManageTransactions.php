3cf54bfdef6c331276c67e3e565d5ece90209c06
FelipeRodrigues05 <fehzin_@outlook.com>
2025-09-03 21:17:43 -0300

    refactor to make it easy to refactor

--- app/Filament/Resources/Transactions/Pages/ManageTransactions.php
+++ app/Filament/Resources/Transactions/Pages/ManageTransactions.php
@@ -33,9 +33,9 @@ final class ManageTransactions extends ManageRecords
     public function getTabs(): array
     {
         return [
-            'all'     => Tab::make('All Transactions')->icon(Heroicon::ListBullet),
-            'income'  => Tab::make('Income')->icon(Heroicon::ArrowTrendingUp)->modifyQueryUsing(fn ($query) => $query->where('type', TransactionTypeEnum::INCOME)),
-            'expense' => Tab::make('Expense')->icon(Heroicon::ArrowTrendingDown)->modifyQueryUsing(fn ($query) => $query->where('type', TransactionTypeEnum::EXPENSE)),
+            'all'          => Tab::make('All Transactions')->icon(Heroicon::ListBullet),
+            'income'       => Tab::make('Income')->icon(Heroicon::ArrowTrendingUp)->modifyQueryUsing(fn ($query) => $query->where('type', TransactionTypeEnum::INCOME)),
+            'expense'      => Tab::make('Expense')->icon(Heroicon::ArrowTrendingDown)->modifyQueryUsing(fn ($query) => $query->where('type', TransactionTypeEnum::EXPENSE)),
             'is_recurrent' => Tab::make('Recurrents')->icon(Heroicon::Clock)->modifyQueryUsing(fn ($query) => $query->where('is_recurrent', true)),
         ];
     }
@@ -48,54 +48,58 @@ final class ManageTransactions extends ManageRecords
         return [
             Action::make('add')
                 ->icon(Heroicon::PlusCircle)
-                ->steps([
-                    Step::make('Transaction Info')
-                        ->schema([
-                            Grid::make()
-                                ->columns(2)
-                                ->schema([
-                                    TextInput::make('value')
-                                        ->required()
-                                        ->numeric()
-                                        ->prefix('R$ ')
-                                        ->placeholder('1.000,00')
-                                        ->live(),
-
-                                    Select::make('category')
-                                        ->options(Category::query()->fromUser()->pluck('name', 'id'))
-                                        ->required()
-                                        ->native(false),
-                                ]),
-                            Grid::make()->columns(2)
-                                ->schema([
-                                    DatePicker::make('transaction_date')
-                                        ->required()
-                                        ->native(false)
-                                        ->helperText('If is recurrent this will be the expiration date')
-                                        ->default(now()),
-
-                                    Radio::make('type')
-                                        ->options(['Expense', 'Income'])
-                                        ->required(),
-
-                                    Toggle::make('is_recurrent')
-                                        ->inline(false)
-                                        ->required(),
-                                ]),
-
-                            FileUpload::make('attachment')->disk('s3')->visibility('public')->directory('uploads')->required(),
-                        ]),
-                    Step::make('Cards')
-                        ->schema([
-                            Select::make('card')->options(Card::query()->fromUser()->pluck('name', 'id'))->native(false),
-                        ]),
-                    Step::make('Description')
-                        ->schema([
-                            MarkdownEditor::make('description'),
-                        ]),
-                ])
+                ->steps(self::steps())
                 ->action(fn (array $data) => CreateTransactionAction::handle(collect($data)))
                 ->slideOver(),
+        ];
+    }
+
+    private static function steps(): array {
+        return [
+            Step::make('Transaction Info')
+                ->schema([
+                    Grid::make()
+                        ->columns(2)
+                        ->schema([
+                            TextInput::make('value')
+                                ->required()
+                                ->numeric()
+                                ->prefix('R$ ')
+                                ->placeholder('1.000,00')
+                                ->live(),
+
+                            Select::make('category')
+                                ->options(Category::query()->fromUser()->pluck('name', 'id'))
+                                ->required()
+                                ->native(false),
+                        ]),
+                    Grid::make()->columns(2)
+                        ->schema([
+                            DatePicker::make('transaction_date')
+                                ->required()
+                                ->native(false)
+                                ->helperText('If is recurrent this will be the expiration date')
+                                ->default(now()),
+
+                            Radio::make('type')
+                                ->options(['Expense', 'Income'])
+                                ->required(),
+
+                            Toggle::make('is_recurrent')
+                                ->inline(false)
+                                ->required(),
+                        ]),
+
+                    FileUpload::make('attachment')->disk('s3')->visibility('public')->directory('uploads')->required(),
+                ]),
+            Step::make('Cards')
+                ->schema([
+                    Select::make('card')->options(Card::query()->fromUser()->pluck('name', 'id'))->native(false),
+                ]),
+            Step::make('Description')
+                ->schema([
+                    MarkdownEditor::make('description'),
+                ]),
         ];
     }
 }
