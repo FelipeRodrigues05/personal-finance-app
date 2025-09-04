be35653e5f0b33c8d0d14fad7649a3071dbf5471
FelipeRodrigues05 <fehzin_@outlook.com>
2025-09-03 21:15:26 -0300

    refactor to make it easy to refactor

--- app/Filament/Resources/Transactions/Pages/ViewTransaction.php
+++ app/Filament/Resources/Transactions/Pages/ViewTransaction.php
@@ -2,16 +2,11 @@
 
 namespace App\Filament\Resources\Transactions\Pages;
 
+use App\Actions\Transaction\UpdateTransactionAction;
 use App\Enums\TransactionTypeEnum;
 use App\Filament\Resources\Transactions\TransactionResource;
-use App\Models\Category;
 use Exception;
 use Filament\Actions\EditAction;
-use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
 use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\TextEntry;
@@ -33,58 +28,7 @@ final class ViewTransaction extends ViewRecord
     public function infolist(Schema $schema): Schema
     {
         return $schema
-            ->components([
-                Section::make("Voucher")
-                    ->icon(Heroicon::Photo)
-                    ->schema([
-                        ImageEntry::make('image_path')->hiddenLabel()
-                            ->extraImgAttributes([
-                                'alt'   => 'Voucher',
-                                'style' => 'width: 100%; height: 100%; object-fit: cover;',
-                            ]),
-                    ]),
-
-                Grid::make()->columns(1)->schema([
-                    Section::make("Transaction Details")->afterHeader([
-                        TextEntry::make("transaction_date")->date()->hiddenLabel(),
-                    ])
-                        ->icon(Heroicon::InformationCircle)
-                        ->schema([
-                            Grid::make()->columns(4)
-                                ->schema([
-                                    TextEntry::make('value')
-                                        ->icon(Heroicon::CurrencyDollar)
-                                        ->money('BRL', locale: 'pt-BR')
-                                        ->color('primary')
-                                        ->size(TextSize::Large)
-                                        ->weight(FontWeight::Bold),
-                                    TextEntry::make('category.name')
-                                        ->label('Category')
-                                        ->money('BRL', locale: 'pt-BR'),
-                                    TextEntry::make('type')->color($this->record->type == TransactionTypeEnum::EXPENSE ? 'danger' : 'success'),
-                                    IconEntry::make('is_recurrent')->boolean(),
-                                ]),
-
-                            TextEntry::make('description')->default('No description'),
-                        ])->collapsible(),
-
-                    Section::make('Card Details')
-                        ->icon(Heroicon::CreditCard)
-                        ->visible((bool)$this->record->used_card)
-                        ->afterHeader([
-                            TextEntry::make('card.type')->badge()->hiddenLabel(),
-                        ])
-                        ->schema([
-                            TextEntry::make('card.name')->size(TextSize::Medium)->weight(FontWeight::Medium),
-                            Grid::make()->columns()->schema([
-                                TextEntry::make('card.limit')->money('BRL', locale: 'pt-BR')
-                                    ->weight(FontWeight::Bold)
-                                    ->color('info'),
-                                TextEntry::make('card.used')->money('BRL', locale: 'pt-BR')->color('info'),
-                            ]),
-                        ]),
-                ]),
-            ]);
+            ->components(self::infolistContent());
     }
 
     /**
@@ -93,31 +37,60 @@ final class ViewTransaction extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            EditAction::make()->schema([
-                Section::make('Transaction Receipt')
+            EditAction::make()->icon(Heroicon::PencilSquare)->action(fn (array $data) => UpdateTransactionAction::handle(collect($data)))
+        ];
+    }
+
+    private function infolistContent(): array {
+        return [
+            Section::make("Voucher")
+                ->icon(Heroicon::Photo)
+                ->schema([
+                    ImageEntry::make('image_path')->hiddenLabel()
+                        ->extraImgAttributes([
+                            'alt'   => 'Voucher',
+                            'style' => 'width: 100%; height: 100%; object-fit: cover;',
+                        ]),
+                ]),
+
+            Grid::make()->columns(1)->schema([
+                Section::make("Transaction Details")->afterHeader([
+                    TextEntry::make("transaction_date")->date()->hiddenLabel(),
+                ])
+                    ->icon(Heroicon::InformationCircle)
                     ->schema([
-                        FileUpload::make('image_path')
-                            ->disk('s3')
-                            ->default('image_path')
-                            ->visibility('public')
-                            ->previewable()
-                            ->live(),
-                    ]),
-                Section::make('Transaction Information')
+                        Grid::make()->columns(4)
+                            ->schema([
+                                TextEntry::make('value')
+                                    ->icon(Heroicon::CurrencyDollar)
+                                    ->money('BRL', locale: 'pt-BR')
+                                    ->color('primary')
+                                    ->size(TextSize::Large)
+                                    ->weight(FontWeight::Bold),
+                                TextEntry::make('category.name')
+                                    ->label('Category')
+                                    ->money('BRL', locale: 'pt-BR'),
+                                TextEntry::make('type')->color($this->record->type === TransactionTypeEnum::EXPENSE ? 'danger' : 'success'),
+                                IconEntry::make('is_recurrent')->boolean(),
+                            ]),
+
+                        TextEntry::make('description')->default('No description'),
+                    ])->collapsible(),
+
+                Section::make('Card Details')
+                    ->icon(Heroicon::CreditCard)
+                    ->visible((bool) $this->record->used_card)
+                    ->afterHeader([
+                        TextEntry::make('card.type')->badge()->hiddenLabel(),
+                    ])
                     ->schema([
-                        TextInput::make('value'),
-                        DatePicker::make('transaction_date'),
-                        Select::make('type')->options([
-                            "income"  => "Income",
-                            "expense" => "Expense",
-                        ])->native(false),
-                        Select::make('category')
-                            ->options(Category::query()->fromUser()->pluck('name'))
-                            ->native(false),
-                    ]),
-                Section::make('Transaction Description')
-                    ->schema([
-                        MarkdownEditor::make('description'),
+                        TextEntry::make('card.name')->size(TextSize::Medium)->weight(FontWeight::Medium),
+                        Grid::make()->columns()->schema([
+                            TextEntry::make('card.limit')->money('BRL', locale: 'pt-BR')
+                                ->weight(FontWeight::Bold)
+                                ->color('info'),
+                            TextEntry::make('card.used')->money('BRL', locale: 'pt-BR')->color('info'),
+                        ]),
                     ]),
             ]),
         ];
