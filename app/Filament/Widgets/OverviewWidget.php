6ae0342ad3f8c06916add141daf9c32c010d0527
FelipeRodrigues05 <fehzin_@outlook.com>
2025-09-03 21:14:53 -0300

    changed all parameters type from string to Carbon and added a cast to float in returns

--- app/Filament/Widgets/OverviewWidget.php
+++ app/Filament/Widgets/OverviewWidget.php
@@ -1,9 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Filament\Widgets;
 
 use App\Enums\TransactionTypeEnum;
 use App\Models\Transaction;
+use Carbon\Carbon;
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -15,11 +16,12 @@ final class OverviewWidget extends StatsOverviewWidget
 
     protected function getStats(): array
     {
-        $startDate = $this->pageFilters['startDate'] ?? now()->startOfMonth();
-        $endDate   = $this->pageFilters['endDate']   ?? now();
+        $startDate = isset($this->pageFilters['startDate']) ? Carbon::parse($this->pageFilters['startDate']) : now()->startOfMonth();
+        $endDate   = isset($this->pageFilters['endDate']) ? Carbon::parse($this->pageFilters['endDate']) : now();
 
         return [
-            Stat::make('Spent', Number::currency($this->calculateExpenses($startDate, $endDate), 'BRL', 'pt-BR'))
+            Stat::make('Spent', Number::currency($this->calculateExpenses($startDate, $endDate), 'BRL',
+                'pt-BR'))
                 ->description('Total Spent'),
 
             Stat::make('Income', Number::currency($this->calculateIncome($startDate, $endDate), 'BRL', 'pt-BR'))
@@ -36,28 +38,28 @@ final class OverviewWidget extends StatsOverviewWidget
         ];
     }
 
-    private function calculateExpenses(string $startDate, string $endDate): false|string
+    private function calculateExpenses(Carbon $startDate, Carbon $endDate): false|float
     {
-        return Transaction::query()->fromUser()->where('type', TransactionTypeEnum::EXPENSE)->whereBetween('transaction_date', [$startDate, $endDate])->sum('value');
+        return (float) Transaction::query()->fromUser()->where('type', TransactionTypeEnum::EXPENSE)->whereBetween('transaction_date', [$startDate, $endDate])->sum('value');
     }
 
-    private function calculateIncome(string $startDate, string $endDate): false|string
+    private function calculateIncome(Carbon $startDate, Carbon $endDate): false|float
     {
-        return  Transaction::query()->fromUser()->where('type', TransactionTypeEnum::INCOME)->whereBetween('transaction_date', [$startDate, $endDate])->sum('value');
+        return (float) Transaction::query()->fromUser()->where('type', TransactionTypeEnum::INCOME)->whereBetween('transaction_date', [$startDate, $endDate])->sum('value');
     }
 
-    private function getTotalTransactions(string $startDate, string $endDate): float
+    private function getTotalTransactions(Carbon $startDate, Carbon $endDate): float
     {
-        return  Transaction::query()->fromUser()->whereBetween('transaction_date', [$startDate, $endDate])->count();
+        return Transaction::query()->fromUser()->whereBetween('transaction_date', [$startDate, $endDate])->count();
     }
 
-    private function getTotalExpenses(string $startDate, string $endDate): float
+    private function getTotalExpenses(Carbon $startDate, Carbon $endDate): float
     {
-        return  Transaction::query()->fromUser()->where('type', TransactionTypeEnum::EXPENSE)->whereBetween('transaction_date', [$startDate, $endDate])->count();
+        return Transaction::query()->fromUser()->where('type', TransactionTypeEnum::EXPENSE)->whereBetween('transaction_date', [$startDate, $endDate])->count();
     }
 
-    private function getTotalIncomes(string $startDate, string $endDate): float
+    private function getTotalIncomes(Carbon $startDate, Carbon $endDate): float
     {
-        return  Transaction::query()->fromUser()->where('type', TransactionTypeEnum::INCOME)->whereBetween('transaction_date', [$startDate, $endDate])->count();
+        return Transaction::query()->fromUser()->where('type', TransactionTypeEnum::INCOME)->whereBetween('transaction_date', [$startDate, $endDate])->count();
     }
 }
