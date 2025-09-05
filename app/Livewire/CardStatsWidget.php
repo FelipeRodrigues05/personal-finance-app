b8a1dbe67252b5259f8506424a132b32a0496696
FelipeRodrigues05 <fehzin_@outlook.com>
2025-09-05 11:32:35 -0300

    added to cards page

---
+++ app/Livewire/CardStatsWidget.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Card;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Number;
+
+class CardStatsWidget extends StatsOverviewWidget
+{
+    protected function getStats(): array
+    {
+        $cards = $this->getUserCards();
+
+        return [
+            Stat::make(__("Total Cards"), $cards->count()),
+            Stat::make(__("Total Limit"), Number::currency($cards->sum('limit'), 'BRL', 'pt_BR')),
+            Stat::make(__("Total Used"), Number::currency($cards->sum('used'), 'BRL', 'pt_BR')),
+        ];
+    }
+
+    private function getUserCards(): Collection {
+        return Card::query()->fromUser()->get();
+    }
+}
